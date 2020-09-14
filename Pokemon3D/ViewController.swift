2015be61a8af1951e781f4bd083f50402cd921bd
Mike <mnguyen287@ucmerced.edu>
2020-09-14 12:31:01 -0700

    initial commit

---
+++ Pokemon3D/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Pokemon3D
+//
+//  Created by Mike Nguyen on 9/8/20.
+//  Copyright © 2020 Mike Nguyen. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images Successfully Added")
+        }
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate Methods
+  
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            var imageName = String()
+            if imageAnchor.referenceImage.name! == "eevee-card" {
+                imageName = "eevee"
+            } else if imageAnchor.referenceImage.name! == "oddish-card" {
+                imageName = "oddish"
+            }
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor.init(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.scnassets/\(imageName).scn") {
+                if let pokeNode = pokeScene.rootNode.childNodes.first {
+                    
+                    pokeNode.eulerAngles.x = .pi / 2
+                    planeNode.addChildNode(pokeNode)
+                    
+                }
+            }
+        }
+        
+        return node
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Pokemon in 3D
+
+Used ARKit to show Pokemon on simple playing cards.  
+Simply try to make playing card identifiable using image recognition and then project a plane that your Pokemon models can rest on. Using basic rotational math, you can get them to appear correctly. I used Eevee and Oddish.
+
+![Alt text](https://github.com/mikedinhnguyen/Pokemon3D/blob/master/images/IMG_3041.jpg)
+![Alt text](https://github.com/mikedinhnguyen/Pokemon3D/blob/master/images/IMG_3042.jpg)
